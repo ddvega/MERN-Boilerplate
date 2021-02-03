caf185ee67e957bf4e55fd79059d9011ece76ce2
ddvega <daviddvega@gmail.com>
2021-02-02 20:53:06 -0600

    more ui changes

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,157 +1,71 @@
-import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import Toolbar from '@material-ui/core/Toolbar';
-import { useAuth } from '../contexts/AuthContext';
-import Drawer from '@material-ui/core/Drawer';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import Badge from '@material-ui/core/Badge';
-import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import NotificationsIcon from '@material-ui/icons/Notifications';
+import React, { useState, useEffect } from 'react';
+import { Button } from './Button';
 import { Link } from 'react-router-dom';
-import { Navlist } from './Navlist';
-import { makeStyles } from '@material-ui/core/styles';
-import clsx from 'clsx';
-import { TrainRounded } from '@material-ui/icons';
 import './Navbar.css';
 
-const drawerWidth = 240;
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    padding: theme.spacing(2),
-    display: 'flex',
-    overflow: 'auto',
-    flexDirection: 'column',
-  },
-  title: {
-    flexGrow: 1,
-  },
-
-  appBarShift: {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  menuButtonHidden: {
-    display: 'none',
-  },
-  drawerPaper: {
-    position: 'relative',
-    whiteSpace: 'nowrap',
-    width: drawerWidth,
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  drawerPaperClose: {
-    overflowX: 'hidden',
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    width: theme.spacing(7),
-    [theme.breakpoints.up('sm')]: {
-      width: theme.spacing(9),
-    },
-  },
-  toolbar: {
-    paddingRight: 24,
-  },
-  toolbarIcon: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    padding: '0 8px',
-    ...theme.mixins.toolbar,
-  },
-}));
-
 export function Navbar() {
-  const classes = useStyles();
-  const { currentUser } = useAuth();
-  const [open, setOpen] = React.useState(false);
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
-  // handle opening and closing of app drawer
-  const drawerOpen = () => {
-    setOpen(true);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
   };
 
-  const drawerClose = () => {
-    setOpen(false);
-  };
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener('resize', showButton);
 
   return (
     <>
-      <AppBar className={clsx(open && classes.appBarShift, 'navbar')}>
-        <Toolbar className={classes.toolbar}>
-          <IconButton
-            edge="start"
-            color="inherit"
-            aria-label="open drawer"
-            onClick={drawerOpen}
-            className={clsx(classes.menuButton, open && classes.menuButtonHidden)}
-          >
-            <MenuIcon />
-          </IconButton>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <div className="menu-icon" onClick={handleClick}>
+            <i
+              className={click ? 'fas fa-times' : 'fas fa-bars'}
+              style={{ color: '#26c6da' }}
+            />
+          </div>
 
-          <Typography
-            component="h1"
-            variant="h6"
-            color="inherit"
-            noWrap
-            className={classes.title}
-          >
-            <Link to="/" className="navbar-logo">
-              <p style={{ paddingRight: '10px' }}> App</p>{' '}
-              <i className="fab fa-galactic-republic" />
-            </Link>
-          </Typography>
-          <IconButton color="inherit">
-            <Badge badgeContent={4} color="secondary">
-              <NotificationsIcon />
-            </Badge>
-          </IconButton>
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+            <p style={{marginRight:'10px'}}>FlickJunkies</p>
+            <i class="fas fa-brain" />
+          </Link>
 
-          {currentUser && (
-            <Button color="inherit">{`Hello,  ${localStorage.getItem(
-              'username',
-            )}`}</Button>
-          )}
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                <i class="fas fa-home">{`  Home`}</i>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/search-users" className="nav-links" onClick={closeMobileMenu}>
+                <i class="fas fa-users">{`  Users`}</i>
+              </Link>
+            </li>
 
-          <Typography>
-            <Link to={currentUser ? '/logout' : '/login'} className="nav-links">
-            <p style={{ paddingRight: '10px' }}>{currentUser ? 'logout' : 'login'}</p>
-            </Link>
-          </Typography>
+            <li className="nav-item">
+              <Link to="/update-profile" className="nav-links" onClick={closeMobileMenu}>
+                <i class="fas fa-user-circle">{`  Profile`}</i>
+              </Link>
+            </li>
 
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        variant="temporary"
-        classes={{
-          paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
-        }}
-        open={open}
-      >
-        <div className={classes.toolbarIcon}>
-          <IconButton onClick={drawerClose}>
-            <ChevronLeftIcon />
-          </IconButton>
+            <li className="nav-item">
+              <Link to="/forgot-password" className="nav-links" onClick={closeMobileMenu}>
+                <i class="fas fa-unlock alt">{`  Reset Password`}</i>
+              </Link>
+            </li>
+          </ul>
         </div>
-        <Divider />
-
-        <Navlist drawer={drawerClose} />
-      </Drawer>
+      </nav>
     </>
   );
 }
